756080d843d0c0e6502ce875b7d15ed01ee91d42
Juan Rafael García Blanco <juanrgar@gmail.com>
2018-06-30 19:04:10 +0200

    Implement EventConsumer

---
+++ src/Mdk/Management/EventConsumer.cpp
@@ -0,0 +1,79 @@
+/** This file is part of smp-mdk
+ *
+ * Copyright (C) 2018 Juan R. Garcia Blanco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Mdk/Management/EventConsumer.h"
+
+using namespace ::Smp::Mdk::Management;
+
+EventConsumer::EventConsumer(void)
+{
+}
+
+EventConsumer::~EventConsumer(void)
+{
+    Clear();
+}
+
+const ::Smp::EventSinkCollection* EventConsumer::GetEventSinks(void) const
+{
+    return &(this->m_eventSinks);
+}
+
+::Smp::IEventSink* EventConsumer::GetEventSink(
+        ::Smp::String8 name) const
+{
+    if (name == NULL) {
+        return NULL;
+    }
+
+    ::Smp::EventSinkCollection::const_iterator it(this->m_eventSinks.begin());
+    ::Smp::EventSinkCollection::const_iterator endIt(this->m_eventSinks.end());
+
+    ::Smp::IEventSink* es = NULL;
+
+    while ((es == NULL) && (it != endIt)) {
+        if (::strcmp(name, (*it)->GetName()) == 0) {
+            es = *it;
+        }
+
+        ++it;
+    }
+
+    return es;
+}
+
+void EventConsumer::AddEventSink(
+        ::Smp::IEventSink* eventSink)
+{
+    if (eventSink == NULL) {
+        return;
+    }
+
+    ::Smp::IEventSink* dup = GetEventSink(eventSink->GetName());
+
+    if (dup != NULL) {
+        return;
+    }
+
+    this->m_eventSinks.push_back(eventSink);
+}
+
+void EventConsumer::Clear(void)
+{
+    this->m_eventSinks.clear();
+}
---
+++ src/Mdk/Management/EventConsumer.h
@@ -0,0 +1,56 @@
+/** This file is part of smp-mdk
+ *
+ * Copyright (C) 2018 Juan R. Garcia Blanco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MDK_MANAGEMENT_EVENTCONSUMER_H_
+#define MDK_MANAGEMENT_EVENTCONSUMER_H_
+
+#include "Smp/Management/IEventConsumer.h"
+
+namespace Smp
+{
+    namespace Mdk
+    {
+        namespace Management
+        {
+            class EventConsumer :
+                public virtual ::Smp::Management::IEventConsumer
+            {
+                public:
+                    EventConsumer(void);
+
+                    virtual ~EventConsumer(void);
+
+                    virtual const ::Smp::EventSinkCollection* GetEventSinks(void) const;
+
+                    virtual ::Smp::IEventSink* GetEventSink(
+                            ::Smp::String8 name) const;
+
+                protected:
+                    void AddEventSink(
+                            ::Smp::IEventSink* eventSink);
+
+                    void Clear(void);
+
+                private:
+                    ::Smp::EventSinkCollection m_eventSinks;
+            };
+        }
+    }
+}
+
+#endif  // MDK_MANAGEMENT_EVENTCONSUMER_H_
